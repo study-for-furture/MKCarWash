c6dc8f70352fdd5efb1939bced8a6d54897b0379
DESKTOP-035QAMS\jhchoi <timulys@gmail.com>
2018-11-09 14:55:32 +0900

    MK Car Wash 최초 커밋

---
+++ src/main/java/com/mk/CarwashApplication.java
@@ -0,0 +1,12 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarwashApplication.class, args);
+	}
+}
